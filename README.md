e5e6f04455beaa917e7f5a9cc4c22ca31eb8fd8b
eastsideslc <mcsnet@yahoo.com>
2018-08-11 13:58:16 +1000

    added README requirements for AWS cli and change regions to be a variable

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ami_finder
 Find your ami_id's for all regions for your products in the AWS Marketplace
+
+### Requirements ###
+You will need the AWS cli tool installed and configured for the AWS account you want to query.  
--- ami_finder.sh
+++ ami_finder.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-for i in $(aws ec2 describe-regions --output text | cut -f3 | sort); 
+# Variables
+regions=$(aws ec2 describe-regions --output text | cut -f3 | sort)
+
+for i in $regions; 
 	do 
-		echo "$i $(aws ec2 describe-images --owners 'aws-marketplace' --filters "Name=name,Values=*$1*" --query 'sort_by(Images, &CreationDate)[-1].[ImageId] ' --region $i)";
+		echo "$i $(aws ec2 describe-images --owners 'aws-marketplace' \
+			--filters "Name=name,Values=*$1*" \
+			--query 'sort_by(Images, &CreationDate)[-1].[ImageId] ' --region $i)";
 	done
