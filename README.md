75de67967241faa356ae8b2001874045ac29dd14
eastsideslc <mcsnet@yahoo.com>
2018-08-11 14:07:39 +1000

    One more cleanup of the script and added a space to README codeblock

--- README.md
+++ README.md
@@ -7,4 +7,7 @@ Find your ami_id's for all regions for your products in the AWS Marketplace
 
 ### How to use ami_finder.sh ###
 To will need to pass the script a valid AWS Marketplace Product Code:
+
 ```./ami_finder.sh 9566ccc2-d817-4c8e-a2e9-b962addec48b```
+
+
--- ami_finder.sh
+++ ami_finder.sh
@@ -4,10 +4,10 @@
 regions=$(aws ec2 describe-regions --output text | cut -f3 | sort)
 
 for i in $regions; 
-	do 
-		echo "$i $(aws ec2 describe-images \
-			--owners 'aws-marketplace' \
-			--filters "Name=name,Values=*$1*" \
-			--query 'sort_by(Images, &CreationDate)[-1].[ImageId] ' \
-			--region $i)";
-	done
+do 
+	echo "$i $(aws ec2 describe-images \
+		--owners 'aws-marketplace' \
+		--filters "Name=name,Values=*$1*" \
+		--query 'sort_by(Images, &CreationDate)[-1].[ImageId] ' \
+		--region $i)";
+done
