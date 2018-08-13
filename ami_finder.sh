7098d358059c62c49d183b4870f88c7e442d6042
eastsideslc <mcsnet@yahoo.com>
2018-08-13 13:48:20 +1000

    changed the exit code to 1

--- ami_finder.sh
+++ ami_finder.sh
@@ -2,7 +2,7 @@
 # copyright 2018 paulm@securestack.com.au
 # ami_finder finds the ami_ids for all regions for an aws marketplace product
 
-if [ -z $1 ]; then echo "You must provide a Product ID."; echo "Example: ami_finder.sh 9566ccc2-d817-4c8e-a2e9-b962addec48b"; exit 0; fi
+if [ -z $1 ]; then echo "You must provide a Product ID."; echo "Example: ami_finder.sh 9566ccc2-d817-4c8e-a2e9-b962addec48b"; exit 1; fi
 
 regions=$(aws ec2 describe-regions --output text | cut -f3 | sort)
 
